37f81b3aa74eacf4387dd59257b458c68561860e
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-03-26 22:54:40 +0100

    dsd: make it work with convolutional layer

--- src/utils.py
+++ src/utils.py
@@ -5,29 +5,27 @@ import tensorflow_probability as tfp
 
 def plot_wb(model, ranges=None):
     
-    for i, l in enumerate(model.layers[:-1]):
+    # According to paper, first conv layer is ignored.
+    for i, l in enumerate(model.layers[1:]):
+        w = l.get_weights()[0].flatten()
+        b = l.get_weights()[1].flatten()
+
         # Plot.
         fig = plt.figure(figsize=(15,5))
 
         fig.add_subplot(1,2,1)
         plt.title("weights " + l.name)
-        plt.ylim(top=20000)
-        w = l.get_weights()[0].flatten()
         plt.hist(w, bins=100, range=ranges);
 
         fig.add_subplot(1,2,2)
         plt.title("biases " + l.name)
-        plt.ylim(top=20)
-        b = l.get_weights()[1].flatten()
         plt.hist(b, bins=100, range=ranges);
 
 def wb_non_zero_percentage(model):
-    pos_last_layer = len(model.layers) - 1
 
     for i, l in enumerate(model.layers):
-
         # Take all dense layer except last one,
-        if i != pos_last_layer and  i % 2 == 0:
+        if i > 0 and ("conv" in l.name or "dense" in l.name):
             w, b = model.layers[0].get_weights()
 
             non_zero_w = tf.math.count_nonzero(w)
